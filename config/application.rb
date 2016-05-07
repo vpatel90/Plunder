18e5d1632ace9f38c05f0a7b15324e58e3614c19
Vivek Patel <vnp229@gmail.com>
2016-05-07 18:13:48 -0500

    added config line to serve rails assets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Plunder
   class Application < Rails::Application
+    #assets on heroku
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
