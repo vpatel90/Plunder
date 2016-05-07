299356b3a2df6a3e34034d3df31be93af52e63d6
Vivek Patel <vnp229@gmail.com>
2016-05-07 18:21:12 -0500

    changes in prod rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Plunder
   class Application < Rails::Application
-    #assets on heroku
-    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
