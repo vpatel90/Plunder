3b706e956152bbdc9913351cadc6c9d7481f789d
Vivek Patel <vnp229@gmail.com>
2016-05-15 16:07:07 -0500

    migration for player table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513184048) do
+ActiveRecord::Schema.define(version: 20160515210231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 20160513184048) do
     t.string   "portrait"
     t.boolean  "booted",         default: false
     t.integer  "afk_count",      default: 0
+    t.boolean  "turn_started",   default: false
   end
 
   add_index "players", ["game_id"], name: "index_players_on_game_id", using: :btree
