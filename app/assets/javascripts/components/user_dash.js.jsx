59f8bdbf388926734ac5e841d7dc37f50ac6e451
Vivek Patel <vnp229@gmail.com>
2016-05-16 18:14:11 -0500

    changed user dash from polling every second to polling on response

--- app/assets/javascripts/components/user_dash.js.jsx
+++ app/assets/javascripts/components/user_dash.js.jsx
@@ -31,11 +31,12 @@ var UserDash = React.createClass ({
             notificationLog.scrollTop = notificationLog.scrollHeight - notificationLog.clientHeight;
 
        });
+       setTimeout(that.tick(), 500);
 
    },
    componentDidMount: function() {
      this.tick();
-     this.interval = setInterval(this.tick, 1000);
+    //  this.interval = setInterval(this.tick, 1000);
    },
    componentWillUnmount: function() {
      clearInterval(this.interval);
