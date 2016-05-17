a9a1fdbf1eabfc0f0c66c4cae3e8a77a734d2b3e
Vivek Patel <vnp229@gmail.com>
2016-05-17 09:19:28 -0500

    made demo deck shorter, for a super fast 2 player game

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -17,10 +17,10 @@ class Deck < ActiveRecord::Base
 
   def build_sample
     cards = Card.where(id: [1,4,5,6,16,
-                            26,31,34,
-                            38,43,46,
-                            50,54,55,58,
-                            62,66,67,70,])
+                            31,34,
+                            43,46,
+                            55,58,
+                            67,70,])
     build_deck(cards)
 
   end
